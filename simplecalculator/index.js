40015545928df3b677b70237058064ac4f272f22
manju1807 <rmanjunath7692@gmail.com>
2023-02-11 12:10:20 +0530

    final commit

--- simplecalculator/index.js
+++ simplecalculator/index.js
@@ -1,79 +1,62 @@
 const display1 = document.querySelector('.display-1');
 const display2 = document.querySelector('.display-2');
-const temp = document.querySelector('.temp');
-const numbers = document.querySelectorAll('.number');
-const operations = document.querySelectorAll('.operation');
-const equal = document.querySelector('.equal');
-const clearAll = document.querySelector('.all-clear');
-const clearLast = document.querySelector('.last-entity-clear');
+const allButtons = document.querySelectorAll('.button');
+let displayValue = '';
+let firstValue = '';
+let secondValue = '';
+let operator = '';
 
-let dis1Num = '';
-let dis2Num = '';
-let result = null;
-let lastOperation = '';
-let haveDot = false;
+allButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const value = e.target.innerText;
 
-numbers.forEach(number => {
-  number.addEventListener('click', (e) => {
-    if (e.target.innerText === '.' && !haveDot) {
-      haveDot = true;
-    } else if (e.target.innerText === '.' && haveDot) {
-      return;
+        if (value === 'C') {
+            displayValue = '';
+            firstValue = '';
+            secondValue = '';
+            operator = '';
+            display1.innerText = '0';
+            display2.innerText = '0';
+        } else if (value === 'CE') {
+            displayValue = '';
+            display1.innerText = '0';
+        } else if (value === '+' || value === '-' || value === 'X' || value === '/' || value === '%') {
+            firstValue = displayValue;
+            operator = value;
+            displayValue = '';
+            display2.innerText = firstValue + operator;
+        } else if (value === '=') {
+            secondValue = displayValue;
+            displayValue = '';
+            display1.innerText = calculate(firstValue, secondValue, operator);
+            display2.innerText = '';
+        } else {
+            displayValue += value;
+            display1.innerText = displayValue;
+        }
+    });
+});
+
+const calculate = (firstValue, secondValue, operator) => {
+    let result = '';
+    switch (operator) {
+        case '+':
+            result = parseFloat(firstValue) + parseFloat(secondValue);
+            break;
+        case '-':
+            result = parseFloat(firstValue) - parseFloat(secondValue);
+            break;
+        case 'X':
+            result = parseFloat(firstValue) * parseFloat(secondValue);
+            break;
+        case '/':
+            result = parseFloat(firstValue) / parseFloat(secondValue);
+            break;
+        case '%':
+            result = parseFloat(firstValue) % parseFloat(secondValue);
+            break;
+        default:
+            return;
     }
-    dis2Num += e.target.innerText;
-    display2.innerText = dis2Num;
-  });
-});
-
-operations.forEach(operation => {
-  operation.addEventListener('click', (e) => {
-    if (!dis2Num) return;
-    haveDot = false;
-    const operationName = e.target.innerText;
-    if (dis1Num && dis2Num && lastOperation) {
-      performMathOperation();
-    } else {
-      result = parseFloat(dis2Num);
-    }
-    clearVars(operationName);
-    lastOperation = operationName;
-  });
-});
-
-function clearVars(name = '') {
-  dis1Num += dis2Num + ' ' + name + ' ';
-  display1.innerText = dis1Num;
-  display2.innerText = '';
-  dis2Num = '';
-  temp.innerText = result;
+    return result;
 }
-
-function performMathOperation() {
-  if (lastOperation === 'X') {
-    result = parseFloat(result) * parseFloat(dis2Num);
-  } else if (lastOperation === '+') {
-    result = parseFloat(result) + parseFloat(dis2Num);
-  } else if (lastOperation === '-') {
-    result = parseFloat(result) - parseFloat(dis2Num);
-  } else if (lastOperation === '/') {
-    result = parseFloat(result) / parseFloat(dis2Num);
-  } else if (lastOperation === '%') {
-    result = parseFloat(result) % parseFloat(dis2Num);
-  }
-}
-
-equal.addEventListener('click', (e) => {
-  if (!dis1Num || !dis2Num) return;
-  haveDot = false;
-  performMathOperation();
-  clearVars();
-  display2.innerText = result;
-  temp.innerText = '';
-  dis2Num = result;
-  dis1Num = '';
-});
-
-clearAll.addEventListener('click', (e) => {
-  display1.innerText = '';
-  display2.innerText = '';
-});
