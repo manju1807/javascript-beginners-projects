92f8b7407fc53ffb3c41c91357b61f8ba310bf31
manju1807 <rmanjunath18@outlook.om>
2023-02-21 22:26:36 +0530

    add file

---
+++ programs.js
@@ -0,0 +1,12 @@
+//program1 matching word
+
+function letterFinder(word,match){
+    for (let i=0;i<word.length;i++){
+           if(word[i]==match)
+                 console.log('Found the', match, 'at', i);
+            else
+                console.log('---No match found at', i);
+    }
+}
+
+letterFinder("test","t");
